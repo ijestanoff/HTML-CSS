a3e2484f55f7a86702ca824cb2bf0fdf722874be
Iliyan Dzhestanov <jestanoff@icloud.com>
2024-10-26 15:07:49 +0300

    Exament preparation & Regular Exament

---
+++ Exam-Prep_05_2024/01.SignUp-Forn/highJump/highJump.js
@@ -0,0 +1,28 @@
+function HighJump(input) {
+    let index = 0;
+    let expectedJump = Number(input[index++]);
+    let failCount = 0;
+    let barHeight = expectedJump - 30;
+    let currentJump = 0;
+    let succeeded = false;
+    while (true) {
+        currentJump = Number(input[index++]);
+        if (currentJump > barHeight) {
+            if (barHeight >= expectedJump) {
+                succeeded = true;
+                break;
+            }
+            barHeight += 5;
+            failCount = 0;
+        } else {
+            if ((++failCount) === 3) {
+                break;
+            }
+        }
+    }
+    if (succeeded) {
+        return console.log(`Tihomir succeeded, he jumped over ${barHeight}cm after ${index - 1} jumps.`);
+    }
+    console.log(`Tihomir failed at ${barHeight}cm after ${index - 1} jumps.`);
+}
+
